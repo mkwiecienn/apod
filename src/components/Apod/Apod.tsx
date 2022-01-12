82bbc95f1b7c0c043b06d1c2a2af3903697a42f9
mkwiecien <kwiecienm@icloud.com>
2022-01-12 17:14:02 +0100

    add loader when img is still fetching

--- src/components/Apod/Apod.tsx
+++ src/components/Apod/Apod.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { APOD } from '../../data/apod';
 import './Apod.scss';
 
@@ -5,31 +6,53 @@ interface ApodProps {
 	apod: APOD;
 }
 
-const Apod = ({ apod }: ApodProps) => (
-	<div className="Apod">
-		<table className="details-table">
-			<tbody>
-				<tr>
-					<td />
-					<td className="apod-img-wrapper">
-						<img className="apod-img" alt={apod.title} src={apod.url} />
-					</td>
-				</tr>
-				<tr>
-					<td>title</td>
-					<td>{apod.title}</td>
-				</tr>
-				<tr>
-					<td>date</td>
-					<td>{apod.date}</td>
-				</tr>
-				<tr>
-					<td>explanation</td>
-					<td>{apod.explanation}</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-);
+const Apod = ({ apod }: ApodProps) => {
+	const [ loading, setLoading ] = useState<boolean>(false);
+
+	useEffect(
+		() => {
+			setLoading(true);
+		},
+		[ apod.url ]
+	);
+
+	const stopLoader = () => {
+		setLoading(false);
+	};
+
+	return (
+		<div className="Apod">
+			<table className="details-table">
+				<tbody>
+					<tr>
+						<td />
+						<td className="apod-img-wrapper">
+							{loading && <p className="loader">Loading...</p>}
+							<img
+								className={`apod-img ${loading ? 'loading' : ''}`}
+								alt={apod.title}
+								src={apod.url}
+								onLoad={stopLoader}
+								onError={stopLoader}
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td>title</td>
+						<td>{apod.title}</td>
+					</tr>
+					<tr>
+						<td>date</td>
+						<td>{apod.date}</td>
+					</tr>
+					<tr>
+						<td>explanation</td>
+						<td>{apod.explanation}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
+};
 
 export default Apod;
